d1be5955ea235e9f41f7e8595aa5155c9861f7f7
Jeanne <jeanne.bnvt@gmail.com>
2026-02-14 22:07:39 +0100

    update figure 1c

--- src/figure_1c.py
+++ src/figure_1c.py
@@ -27,7 +27,7 @@ batch_size = config["batch_size"]
 
 input_dim = X_test.shape[1:] #(c,h,w)
 
-model_weights_path = "../models/model_minst_0.pth"
+model_weights_path = "../models/model_minst_18.pth"
 model = ReconstructionNet(input_dim,num_classes,is_image)
 model.load_state_dict(torch.load(model_weights_path))
 model.eval()
@@ -44,15 +44,21 @@ with torch.no_grad():
     for x, y in test_loader:
         
         probs, r_list, wre = model(x)
+        
         preds = torch.argmax(probs, dim=1)
 
             
         label = y[0].item()
         pred_label = preds[0].item()
 
-        print(label, pred_label)
         if label != pred_label:
-            dico = {"label": label, "pred_label": pred_label, "recons": r_list[label], "image": x[0], "recons_by_pred": r_list[pred_label]}
+            weighted_rec_err = wre[0][pred_label] * r_list[pred_label]
+            dico = {"label": label, 
+                    "pred_label": pred_label, 
+                    "recons_label": r_list[label], 
+                    "recons_pred": r_list[pred_label],
+                    "weighted_rec_err":weighted_rec_err,
+                    "image": x[0] }
             images.append(dico)
             if len(images) == num_misclassified:
                 break
@@ -62,29 +68,34 @@ with torch.no_grad():
    
 
  
-fig, axes = plt.subplots(len(images),4,figsize=(10, 10*len(images)))
+fig, axes = plt.subplots(len(images),4,figsize=(12, 5*len(images)))
 for i in range(len(images)):
+    label= images[i]["label"]
+    pred_label= images[i]["pred_label"]
+    recons_label = images[i]["recons_label"]
+    recons_pred = images[i]["recons_pred"]
+    wre = images[i]["weighted_rec_err"]
     image = images[i]["image"]
-    recons = images[i]["recons"]
-    pred_label   = images[i]["pred_label"]
-    true_label   = images[i]["label"] 
-    recons_by_pred = images[i]["recons_by_pred"]
+
     axes[i,0].imshow(image.permute(1,2,0).numpy(), cmap='gray')
-    axes[i,0].set_title(f"true: {label}, pred:{pred_label}", fontsize = 30)
+    axes[i,0].set_title(f"true: {label}, pred:{pred_label}", )
     axes[i,0].axis('off')
 
-    mask_recons = torch.where(recons>0.5,0,1)
+    minr = torch.min(wre)
+    maxr = torch.max(wre)
+    wre = (wre - minr)/(maxr-minr)
+    mask_recons = torch.where(wre>0.7,0,1)
 
     axes[i,1].imshow(image.reshape(28,28), cmap='gray')
-    axes[i,1].imshow(mask_recons.reshape(28,28), cmap='Set1', alpha=0.5)
+    axes[i,1].imshow(mask_recons.reshape(28,28), cmap = "Set1",alpha=0.5)
     axes[i,1].set_title("erreur recons")
     axes[i,1].axis('off')
 
-    axes[i,2].imshow(recons.reshape(28,28), cmap='gray')
+    axes[i,2].imshow(recons_label.reshape(28,28), cmap='gray')
     axes[i,2].set_title(f"recons par ae {label}")
     axes[i,2].axis('off')
     
-    axes[i,3].imshow(recons_by_pred.reshape(28,28), cmap='gray')
+    axes[i,3].imshow(recons_pred.reshape(28,28), cmap='gray')
     axes[i,3].set_title(f"reconspar ae {pred_label}")
     axes[i,3].axis('off')
     
