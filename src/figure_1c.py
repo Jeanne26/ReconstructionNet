97a2275e4b0544a1b652897fd18a82940adf7125
Jeanne <jeanne.bnvt@gmail.com>
2026-02-14 17:09:41 +0100

    1_c not working

--- src/figure_1c.py
+++ src/figure_1c.py
@@ -27,46 +27,66 @@ batch_size = config["batch_size"]
 
 input_dim = X_test.shape[1:] #(c,h,w)
 
-model_weights_path = "../models/model_minst_4.pth"
+model_weights_path = "../models/model_minst_0.pth"
 model = ReconstructionNet(input_dim,num_classes,is_image)
 model.load_state_dict(torch.load(model_weights_path))
 model.eval()
 
 test_dataset = TensorDataset(X_test, y_test)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
 #get 3 misclassified images
-num_misclassified = 1
-images = {}
-recons = {}
+num_misclassified = 3
+images = []
+
+# recons = {}
 with torch.no_grad():
     for x, y in test_loader:
+        
         probs, r_list, wre = model(x)
-        print(x.shape,probs.shape, r_list[0].shape, wre.shape)
         preds = torch.argmax(probs, dim=1)
-        for i in range(x.size(0)):
-            label = y[i].item()
-            pred_label = preds[i].item()
-            if label != pred_label and label not in images:
-                images[label] = x[i]
-                recons[label] =torch.sqrt( r_list[label][pred_label]) +x[i]
+
+            
+        label = y[0].item()
+        pred_label = preds[0].item()
+
+        print(label, pred_label)
+        if label != pred_label:
+            dico = {"label": label, "pred_label": pred_label, "recons": r_list[label], "image": x[0], "recons_by_pred": r_list[pred_label]}
+            images.append(dico)
             if len(images) == num_misclassified:
                 break
 
-        if len(images) == num_classes:
+        if len(images) == num_misclassified:
             break
-        
-fig, axes = plt.subplots(1,2)
-axes[0].imshow(images[0].permute(1,2,0).numpy(), cmap='gray')
-axes[0].set_title("Original Image")
-axes[0].axis('off')
+   
 
-mask_recons = torch.where(recons[0]>0.015,1,0)
+ 
+fig, axes = plt.subplots(len(images),4,figsize=(10, 10*len(images)))
+for i in range(len(images)):
+    image = images[i]["image"]
+    recons = images[i]["recons"]
+    pred_label   = images[i]["pred_label"]
+    true_label   = images[i]["label"] 
+    recons_by_pred = images[i]["recons_by_pred"]
+    axes[i,0].imshow(image.permute(1,2,0).numpy(), cmap='gray')
+    axes[i,0].set_title(f"true: {label}, pred:{pred_label}", fontsize = 30)
+    axes[i,0].axis('off')
 
-axes[1].imshow(images[0].reshape(28,28), cmap='gray')
-axes[1].imshow(mask_recons.reshape(28,28), cmap='Set1', alpha= 0.5)
-axes[1].set_title("Reconstruction Error")
-axes[1].axis('off')
+    mask_recons = torch.where(recons>0.5,0,1)
 
+    axes[i,1].imshow(image.reshape(28,28), cmap='gray')
+    axes[i,1].imshow(mask_recons.reshape(28,28), cmap='Set1', alpha=0.5)
+    axes[i,1].set_title("erreur recons")
+    axes[i,1].axis('off')
+
+    axes[i,2].imshow(recons.reshape(28,28), cmap='gray')
+    axes[i,2].set_title(f"recons par ae {label}")
+    axes[i,2].axis('off')
+    
+    axes[i,3].imshow(recons_by_pred.reshape(28,28), cmap='gray')
+    axes[i,3].set_title(f"reconspar ae {pred_label}")
+    axes[i,3].axis('off')
+    
 plt.tight_layout()
 plt.savefig("../figures/tmp.png")
